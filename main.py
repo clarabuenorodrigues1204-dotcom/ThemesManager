089278e7ad4322ef159f04ba9d79514cfd6fc4ed
Clara Bueno Rodrigues <clarabuenorodrigues1204@gmail.com>
2026-08-03 03:18:27 -0300

    feat: adiciona validação da escolha de tema

--- main.py
+++ main.py
@@ -2,21 +2,17 @@ from os import system
 system('cls')
 
 import dados.temas as temas
-import interface.interface_variacao as interface_variacao
 import json
-
-arquivo = open("json/config.json" , "r")
-config = json.load(arquivo) #faz a conversão de json para python para que o python consiga manipular
-arquivo.close()
-
-print(config)
 
 titulo = "THEMES MANAGER" #Mostra o título
 
 print(f'{titulo:=^40}')
 
-for i, tema in enumerate(temas.lista_temas, start=1):#Mostra o tema
+for i, tema in enumerate(temas.temas, start=1):#Mostra o tema
     print(f'[{i}] - {tema}')
 
 opcao_usuario = int(input('\nQual é a opção desejada? '))
 
+while opcao_usuario < 1 or opcao_usuario >(len(temas.temas)):
+    print('Opção inválida!')
+    opcao_usuario = int(input('Escolha uma opção válida: '))
