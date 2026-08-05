5a38ca363693cd65df2490e4d2b9c0331648cf83
Clara Bueno Rodrigues <clarabuenorodrigues1204@gmail.com>
2026-08-04 22:53:14 -0300

    cria estrutura inicial do ThemeManager
    Estrutura inicial do projeto ThemeManager

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import system
 system('cls')
 
+from time import sleep
 import dados.temas as temas
 import json
 
@@ -13,6 +14,19 @@ for i, tema in enumerate(temas.temas, start=1):#Mostra o tema
 
 opcao_usuario = int(input('\nQual é a opção desejada? '))
 
-while opcao_usuario < 1 or opcao_usuario >(len(temas.temas)):
-    print('Opção inválida!')
-    opcao_usuario = int(input('Escolha uma opção válida: '))
+while True:   
+    
+    try:
+        if opcao_usuario in range(1, len(temas.temas) + 1):
+            
+            break
+        
+        else:
+            print('Opção inválida!')
+            opcao_usuario = int(input('Digite uma opção válida '))
+            
+    except ValueError:
+        print("Digite apenas números.")
+
+print('Aplicando....')
+sleep(1)
