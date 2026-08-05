3acdd7faee9078e1bfedeef07984aad9111b5131
Clara Bueno Rodrigues <clarabuenorodrigues1204@gmail.com>
2026-08-05 00:20:19 -0300

    implementa validação das opções de tema e variação

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ system('cls')
 from time import sleep
 import dados.temas as temas
 import json
+from interface import interface_variacao
 
 titulo = "THEMES MANAGER" #Mostra o título
 
 print(f'{titulo:=^40}')
 
+#Escolha do tema
 for i, tema in enumerate(temas.temas, start=1):#Mostra o tema
     print(f'[{i}] - {tema}')
 
 opcao_usuario = int(input('\nQual é a opção desejada? '))
 
+#Validação da escolha do tema
 while True:   
     
     try:
@@ -27,6 +30,12 @@ while True:
             
     except ValueError:
         print("Digite apenas números.")
+        
+tema_escolhido = list(temas.temas)[opcao_usuario -1]   
+     
+#Escolha da variação do tema
 
+variacao_escolhida = interface_variacao.escolha_variacao(tema_escolhido)
+print(f'Variação escolhida: {variacao_escolhida}')
 print('Aplicando....')
 sleep(1)
